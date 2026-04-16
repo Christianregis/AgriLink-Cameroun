0ecd84fcc665c912d5b78508ae31ec08b2d30b38
Chritianregis <christiancheukap@gmailcom>
2026-04-16 11:12:24 +0100

    First Commit

---
+++ resources/js/app.ts
@@ -0,0 +1,16 @@
+import { createApp, h, DefineComponent } from 'vue'
+import { createInertiaApp } from '@inertiajs/vue3'
+import '../css/app.css'
+
+createInertiaApp({
+    resolve: (name: string) => {
+        const pages = import.meta.glob<DefineComponent>('./Pages/**/*.vue')
+        return pages[`./Pages/${name}.vue`]()
+    },
+
+    setup({ el, App, props, plugin }) {
+        createApp({ render: () => h(App, props) })
+            .use(plugin)
+            .mount(el)
+    },
+})
