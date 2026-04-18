10142ce880c417a383ddb5724635630d31954c1b
Chritianregis <christiancheukap@gmailcom>
2026-04-18 13:38:50 +0100

    Ajout du canavas d'eciture des routes admin, farmer et buyer

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,18 @@ use Inertia\Inertia;
 Route::get('/', function () {
     return Inertia::render('Home');
 });
+
+// Routes pour l'administrateur
+Route::middleware(['role:admin'])->group(function (){
+    // Routes pour l'adminstrateur ici
+});
+
+// Routes pour l'agriculteur
+Route::middleware(['role:farmer'])->group(function (){
+    // Routes ici
+});
+
+// Route pour l'acheteur
+Route::middleware(['role:buyer'])->group(function(){
+    // Routes ici
+});
